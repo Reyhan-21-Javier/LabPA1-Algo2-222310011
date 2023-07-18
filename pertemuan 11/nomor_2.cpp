e847ff33a22d7ed078421a9409a57cf83fb71695
Reyhan-21-Javier <113398583+Reyhan-21-Javier@users.noreply.github.com>
2023-07-18 08:52:53 +0700

    Add files via upload

---
+++ pertemuan 11/nomor_2.cpp
@@ -0,0 +1,4 @@
+Baris ke 6, mendeklarasikan variabel pointer ke tipe data integer
+Baris ke 7, perintah untuk mengalokasikan memori
+Baris ke 9, perintah untuk menggunakan memori yang telah dialokasikan
+Baris ke 12, perintah untuk mendealokasikan memori
