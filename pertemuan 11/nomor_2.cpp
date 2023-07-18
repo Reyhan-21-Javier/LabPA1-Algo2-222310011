13e24281e540b3b78840895439dd3bd79e0838fd
Reyhan-21-Javier <113398583+Reyhan-21-Javier@users.noreply.github.com>
2023-07-18 11:47:25 +0700

    Delete pertemuan 11 directory

--- pertemuan 11/nomor_2.cpp
+++
@@ -1,4 +0,0 @@
-Baris ke 6, mendeklarasikan variabel pointer ke tipe data integer
-Baris ke 7, perintah untuk mengalokasikan memori
-Baris ke 9, perintah untuk menggunakan memori yang telah dialokasikan
-Baris ke 12, perintah untuk mendealokasikan memori
